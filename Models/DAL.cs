9b2d6bff7ece50175ca20f2cc3ce4e86129b15de
jouelelt310 <justin_ouellet@hotmail.com>
2022-03-24 09:34:38 -0400

    Added DAL.cs

---
+++ Models/DAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UsersManager.Models
+{
+    public static class UsersDBDAL
+    { 
+
+    }
+}
