d3918ae8e9493f00305e06c040f70a9ab0aa076a
jouelelt310 <justin_ouellet@hotmail.com>
2022-03-24 09:06:45 -0400

    Added ItemsController.cs and DAL.cs

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnapSack.Controllers
+{
+    public class ItemsController : Controller
+    {
+        // GET: Items
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
