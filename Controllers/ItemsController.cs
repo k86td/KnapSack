6336c1b0cc0b201877ceca60c65aed172dc32d78
Tristan Lépine <tristanlepine14@gmail.com>
2022-03-28 17:31:27 -0400

    implemented Index of the items

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using KnapSack.Models;
+
 namespace KnapSack.Controllers
 {
     public class ItemsController : Controller
     {
+        KnapSackDbEntities DB = new KnapSackDbEntities();
+
         // GET: Items
         public ActionResult Index()
         {
-            return View();
+            return View(DB.Items);
         }
     }
 }
---
+++ Models/ItemsView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace KnapSack.Models
+{
+    [MetadataType(typeof(ItemsView))]
+    public partial class Item
+    {
+    }
+
+    public partial class ItemsView
+    {
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:0} caps")]
+        public decimal prix { get; set; }
+    }
+}
