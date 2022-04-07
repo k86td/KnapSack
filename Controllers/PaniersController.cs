9014ee80c7e318560d9b28c374cc50c594db88c9
jouellet310 <justin_ouellet@hotmail.com>
2022-04-07 12:53:01 -0400

    Added DAL elements for cart, Added PaniersController.cs and its actions.

---
+++ Controllers/PaniersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KnapSack.Models;
+
+namespace KnapSack.Controllers
+{
+    public class PaniersController : Controller
+    {
+        KnapSackDbEntities DB = new KnapSackDbEntities();
+        // GET: Paniers
+        public ActionResult Panier()
+        {
+            return View("ViewName", DB.Paniers);
+        }
+
+        public ActionResult AddToCart(Item item) 
+        {
+            DB.AddItemToCart(item, OnlinePlayers.GetSessionUser());
+            return RedirectToAction("Panier");
+        }
+
+        public ActionResult ModifyQuantity(Item item, int newQuantity)
+        {
+            DB.ModifyCartQuantityItem(item, OnlinePlayers.GetSessionUser(), newQuantity);
+            return RedirectToAction("Panier");
+        } 
+
+        public ActionResult DeleteCart()
+        {
+            DB.DeleteCart(OnlinePlayers.GetSessionUser());
+            return RedirectToAction("Index", "Items");
+        }
+    }
+}
